01f0c8243c4bd5cb1ade0758098952cda2fb12f7
Ervin Randal <erandal@am-h4.msk.21-school.ru>
2020-08-13 14:21:48 +0300

    g

--- ps_check/ps_valgrind.sh
+++ ps_check/ps_valgrind.sh
@@ -66,13 +66,13 @@ echo ""
 for (( count=1; count<4; count++ ))
 do
 	ARG=`ruby -e "puts (-250..249).to_a.shuffle.join(' ')"`
-	echo "ra\nsa" > tmp.pc
-	valgrind --leak-check=full --log-file=erandal cat tmp.pc | tr -s " " "\n" | ./checker $ARG > tmp.ps
+	echo "ra\nsa" > tmp.ps
+	valgrind --leak-check=full --log-file=erandal cat tmp.ps | tr -s " " "\n" | ./checker $ARG > tmp.ps
 	num=$(cat erandal | grep "definitely lost: 0 bytes in 0 blocks" | wc -l | tr -d "[ \t]");
 	num2=$(cat erandal | grep "indirectly lost: 0 bytes in 0 blocks" | wc -l | tr -d "[ \t]");
 	rm -rf erandal
 	rm -rf tmp.ps
-	rm -rf tmp.pc
+	rm -rf tmp.ps
 	if (( num  == 1 )) && (( num2 == 1))
 	then
 		echo "\033[37;1;42m ✓\033[0m" | tr -d "\n"
@@ -86,8 +86,8 @@ echo ""
 # Необычные:
 
 ARG="1 2 3"
-echo "\n" > tmp.pc
-valgrind --leak-check=full --log-file=erandal cat tmp.pc | tr -s " " "\n" | ./checker $ARG 2> tmp.ps
+echo "\n" > tmp.ps
+valgrind --leak-check=full --log-file=erandal cat tmp.ps | tr -s " " "\n" | ./checker $ARG 2> tmp.ps
 num=$(cat erandal | grep "definitely lost: 0 bytes in 0 blocks" | wc -l | tr -d "[ \t]");
 num2=$(cat erandal | grep "indirectly lost: 0 bytes in 0 blocks" | wc -l | tr -d "[ \t]");
 rm -rf erandal
@@ -100,8 +100,8 @@ else
 fi
 
 ARG="1 2 3"
-echo "rar\nsa" > tmp.pc
-valgrind --leak-check=full --log-file=erandal cat tmp.pc | tr -s " " "\n" | ./checker $ARG 2> tmp.ps
+echo "rar\nsa" > tmp.ps
+valgrind --leak-check=full --log-file=erandal cat tmp.ps | tr -s " " "\n" | ./checker $ARG 2> tmp.ps
 num=$(cat erandal | grep "definitely lost: 0 bytes in 0 blocks" | wc -l | tr -d "[ \t]");
 num2=$(cat erandal | grep "indirectly lost: 0 bytes in 0 blocks" | wc -l | tr -d "[ \t]");
 rm -rf erandal
@@ -114,8 +114,8 @@ else
 fi
 
 ARG="1 2 3"
-echo "ra\nsa" > tmp.pc
-valgrind --leak-check=full --log-file=erandal cat tmp.pc | tr -s " " "\n" | ./checker $ARG > tmp.ps
+echo "ra\nsa" > tmp.ps
+valgrind --leak-check=full --log-file=erandal cat tmp.ps | tr -s " " "\n" | ./checker $ARG > tmp.ps
 num=$(cat erandal | grep "definitely lost: 0 bytes in 0 blocks" | wc -l | tr -d "[ \t]");
 num2=$(cat erandal | grep "indirectly lost: 0 bytes in 0 blocks" | wc -l | tr -d "[ \t]");
 rm -rf erandal
